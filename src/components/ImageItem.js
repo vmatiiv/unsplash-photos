588780e07486aeb9770a6182329bb717e49917e1
vmatiiv <matiiv.vasyl97@gmail.com>
2019-10-05 20:27:48 +0300

    fix mobile loading bug

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 function ImageItem({photo}) {
 
-    const img = React.createRef();
-    console.log(photo);
     return (
         <li className="imageItem" >
 
-            <a href={photo.links.download+'?force=true'} download><img ref={img} className="photo"  src={photo.urls.regular} alt={photo.alt_description}></img></a>
+            <a href={photo.links.download+'?force=true'}>
+                <img  className="photo"  src={photo.urls.regular} alt={photo.alt_description}></img>
+            </a>
 
       
-        // </li>
-        //     // {/* <a href={photo.links.download+'?force=true'} download> */}
-        //         // {/* <img ref={img} className="photo" src={photo.urls.regular} alt={photo.alt_description}></img> */}
-        //     // {/* </a> */}
-        // // {/* </div> */}
+         </li>
+
     )
 }
 
